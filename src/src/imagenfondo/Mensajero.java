2118b1197acbb1d3686b1caf2414c79e585503cd
thejeremyan <ujeremyr@gmail.com>
2021-09-22 23:13:03 -0600

    Changes by Felipe

---
+++ src/src/imagenfondo/Mensajero.java
@@ -0,0 +1,16 @@
+package imagenfondo;
+
+// Manda posicion al cliente y al servidor
+
+public class Mensajero {
+    private int pos;
+    public int getNumber() {
+        return this.pos;
+    }
+
+    public void setNumber(int pos) {
+        this.pos = pos;
+    }
+
+}
+
